837f4bf2282cdfd01605eecc7438c6ed56884459
EmmahSamanisi <emmahsamanisi@gmail.com>
2023-02-21 19:37:34 -0800

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphaet(void);
+void print_alphabet_x10(void);
+int islower(int c);
+int isapha(int c);
+int print_sign(int n);
+int abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
